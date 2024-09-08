e9a7d3fd0c08ab28cf7d79ca18978630889689a7
СИГМА <stepantelnov056@gmail.com>
2024-09-08 15:09:55 +0300

    create main py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("netEror")
+import os
+
+from PyQt5.QtCore import Qt
+
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = r'C:\Users\Админ\AppData\Local\Programs\Python\Python311\Lib\site-packages\PyQt5\Qt5\plugins\platforms'
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class CalendarWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle(" ")
+        self.setGeometry(100, 100, 600, 400)
+        self.setStyleSheet("style.css")
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowMaximizeButtonHint)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = CalendarWindow()
+    window.show()
+    sys.exit(app.exec_())
